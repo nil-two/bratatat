3985bf6f5dbd87094d5e7abf187559efb275605a
kusabashira <kusabashira227@gmail.com>
2016-08-19 00:01:19 +0900

    Test bratatat.Do

---
+++ bratatat_test.go
@@ -0,0 +1,31 @@
+package bratatat_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kusabashira/bratatat"
+)
+
+func TestDo(t *testing.T) {
+	n := 0
+	bratatat.Do(func() {
+		n++
+	})
+
+	expect := 3
+	actual := n
+	if actual != expect {
+		t.Errorf("bratatat.Do should does something 3 times")
+	}
+}
+
+func ExampleDo() {
+	bratatat.Do(func() {
+		fmt.Println("BRATATAT!")
+	})
+	// Output:
+	// BRATATAT!
+	// BRATATAT!
+	// BRATATAT!
+}
